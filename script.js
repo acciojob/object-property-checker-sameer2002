fafab5eb7620841774f2ca8165189423560da1d3
sameer sagar <60567209+sameer2002@users.noreply.github.com>
2023-07-18 01:33:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	for(int i=0;i<sampleObject.length;i++){
+  if(key===sampleObject.key){
+    return true;
+  }
+
+  return false;
+}
 }
 
 // Do not change the code below
